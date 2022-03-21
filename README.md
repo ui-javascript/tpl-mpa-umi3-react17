4b7ea0ed90b0559cd194ef935d4c0ad9cc49938f
luo0412 <1095847440@qq.com>
2022-03-22 07:19:44 +0800

    adjust

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # umi project
 
-写的时候还是多路径路由 
-
-最后打包出来MPA @todo
 
 # 常见问题 @bug
 
-- 打包路径有问题
+- 修改entry, 争取打包出来MPA @todo
+- 打包路径publicPath有问题
--- __demo/index.js
+++ __demo/index.js
@@ -11,5 +11,5 @@ const App = () => {
 }
 export default App;
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
 
